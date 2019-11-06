0e4b4f3d43240510873e2b780cad8a88ecd1f306
Eric <Aerelioyanadel@gmail.com>
2019-11-05 21:35:35 -0500

    fixed the stash, code recovered

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require("express")
 const path = require("path")
 const app = express()
+const axios = require('axios')
 
 // JUST FOR DEMO PURPOSES, PUT YOUR ACTUAL API CODE HERE
 app.get('/api/demo', (request, response) => {
@@ -13,7 +14,30 @@ app.get('/api/demo', (request, response) => {
   })
 })
 // END DEMO
+app.get('/deck', async (request, response) => {
+  try{
+      let {data} = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    const deckId = data.deck_id
+    console.log("deckid",deckId)
+    response.json(deckId)
+    // response.send if you are sending an object
+  } catch(e){
+    console.log(e)
+  } 
+})
 
+app.get('/draw', async (request, response) => {
+  try{
+    const deckId = await request.query.deck_id
+    let {data} = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+      // const drawCard = []
+    // const drawCard = data.cards.map( card => (console.log(card)))
+    console.log("Second api call to draw cards", data)    
+  } catch(error){
+    console.log(error)
+  }
+})
+  
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')))
