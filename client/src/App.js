006ce0510f009aec4b280bfa448f650a7ace70e1
Eric <Aerelioyanadel@gmail.com>
2019-11-05 21:01:13 -0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,29 @@ import React from 'react'
 import './App.css'
 
 class App extends React.Component {
-  state = { serverMessage: '' }
+  state = { cards: [], deckId: "" }
 
   componentDidMount(){
-    fetch('/api/demo')
-      .then(response => response.json())
-      .then(data => this.setState({ serverMessage: data.message }))
+    fetch('/deck')
+      .then(response => {return response.json()})
+      .then(data => this.setState({ deckId : data}))
+  }
+
+  drawCard = () => {
+    fetch(`/draw?deck_id=${this.state.deckId}`)
+    .then(data => {return data.json()})
+    .then(res => console.log(res))
+
   }
 
   render(){
     return (
       <div id="demo">
         <h1>Hello from client/src/App.js</h1>
-        <h1>{this.state.serverMessage}</h1>
+        <h1>{this.state.deckId}</h1>
+        <button onClick={this.drawCard}>
+          draw
+        </button>
       </div>
     )
   }
