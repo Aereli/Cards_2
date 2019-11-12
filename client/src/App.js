e7e429d531e3d5825e9909fc17d565cf7228a71d
Eric <Aerelioyanadel@gmail.com>
2019-11-12 10:25:39 -0500

    added-grab-pile

--- client/src/App.js
+++ client/src/App.js
@@ -2,37 +2,100 @@ import React from 'react'
 import './App.css'
 
 class App extends React.Component {
-  state = { cards: [], deckId: "", image: []}
+  state = { cardsObject: [], 
+            deckId:     "", 
+            image:      "",
+            cardRemain: "", 
+            cardValue:  "",
+            playerTwoPile: [],
+            playerTwoCardCode: "",
+            newPile: []
+          }
 
   componentDidMount(){
     fetch('/deck')
       .then(response => {return response.json()})
-      .then(data => this.setState({ deckId : data}))
+      .then(res => this.setState({
+         deckId: res.deckId
+     }))
   }
 
   drawCard = () => {
     fetch(`/draw?deck_id=${this.state.deckId}`)
     .then(response => {return response.json()})
-    .then(data => this.setState({ cards : data }))
+    // .then( res => console.log(res.cards.map(car => car.code)))
+    .then(data => this.setState({cardsObject: data.cards, cardRemain: data.remaining}))
+    const images = this.state.cardsObject.map(img => img.image)
+    const values = this.state.cardsObject.map(val=> val.value)
+    const cardCode = this.state.cardsObject.map(card => card.code)
+    this.setState({ playerTwoCardCode: cardCode})
+    this.setState({ cardValue: values})
+    this.setState({ image : images})
   }
+
+  thePileGrab = () => {
+    fetch(`/draw/?deck_id=${this.state.deckId}/pile/newpile/add/?cards=${this.state.playerTwoCardCode}`)
+    .then(response => response.json())
+    // .then(data => this.setState({deckId: data.deck_id}))
+    .then(data => console.log(data))
+  }
+
+  thePileList = () => {
+    fetch(`/draw/?deck_id=${this.state.deckId}/pile/newpile/list/`)
+    .then(response => response.json())
+    .then(res => console.log(res))
+  }
+
+  // thePileGrab = () => {
+  //   fetch(`/pile/add/?cards=${this.state.playerTwoCardCode}`)
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }
+
+  // thePileList = () => {
+  //   fetch(`/pile/list`)
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }
 
   render(){
     return (
-      <div id="demo">
-        <h1>Hello from client/src/App.js</h1>
-        <h1>{this.state.deckId}</h1>
-          <div>
-            {
-              this.state.cards.map(val=> val.value)
-            }
-
-            {
-              this.state.cards.map( sui => sui.suit )
-            }
+      <div id="egyptian-rat-slap">
+        <nav>
+          <h3>Cards Remaining: {this.state.cardRemain}</h3>
+        </nav>
+          <div className="center-cards">
+            <div className="player-1">
+              {
+                <img src={this.state.image[0]} alt="card">
+                </img>
+              }
+              <div>{this.state.cardValue[0]}</div>
+            </div>
+            <div className="player-2">
+              {
+                <img src={this.state.image[1]} alt="card">
+                </img>
+              } 
+                <div>{this.state.cardValue[1]} </div>   
+            </div>
           </div>
-        <button onClick={this.drawCard}>
-          draw
-        </button>
+          <div className="draw-button-container">
+            <button className="draw-button"
+                    onClick={this.drawCard}> draw 
+            </button>
+          </div>
+          <div player-2-grab>
+              <button onClick={this.thePileGrab}> player 2 grab</button>
+          </div>
+          <div player-2-pile>
+              <button onClick={this.thePileList}> player 2 pile </button>
+          </div>
+          <div>
+              {
+                // console.log(this.state.cardsObject.map(card => card.code))
+              }
+          </div>
       </div>
     )
   }
--- server.js
+++ server.js
@@ -12,27 +12,41 @@ app.get('/deck', async (request, response) => {
   try{
       let {data} = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
     const deckId = data.deck_id
-    console.log("deckid, server side",deckId)
-    response.json(deckId)
-    // response.send if you are sending an object
+  const cardRemain = data.remaining
+  const deckObject = {deckId, cardRemain}
+    // console.log("deckid, server side",deckId)
+    response.send(deckObject)
   } catch(e){
     console.log(e)
   } 
 })
 
+// app.get('/pile', async (request,  response) => {
+//   try{
+//     const deckId = await request.query.deck_id
+//     let {data} = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/pile/`)
+//     // const newPile = data
+//       response.send(data)
+//     // console.log( data)
+//   } catch(error){
+//     console.log(error)
+//   }
+// })
+
 app.get('/draw', async (request, response) => {
   try{
-      const deckId = await request.query.deck_id
-      let {data} = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-    const cardsObject = data.cards
-    // const drawCard = data.cards.map( card => (console.log(card)))
-    console.log("Second api call to draw cards", cardsObject)  
-    response.json(cardsObject)  
+    const deckId = await request.query.deck_id
+    let {data} = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    const cardsObject = data
+    // console.log("Second api call to draw cards", cardsObject)  
+    response.send(cardsObject)  
   } catch(error){
     console.log(error)
   }
 })
-  
+
+
+
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')))
